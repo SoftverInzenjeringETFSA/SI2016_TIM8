397b49a0349397ecd0a39287d6b7f81541320520
Lejla Zečević <lzecevic1@etf.unsa.ba>
2017-05-16 15:21:28 +0200

    Dodan spring projekat

---
+++ code/svc/src/main/java/com/sietf/svc/SvcApplication.java
@@ -0,0 +1,12 @@
+package com.sietf.svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvcApplication.class, args);
+	}
+}
--- code/webapp/app/router.js
+++ code/webapp/app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('početna');
+  this.route('početna', {path: '/'}); //Ucitava pocetnu stranicu pri prvom ulasku na stranicu
   this.route('pregledpatrola');
   this.route('pomoć');
   this.route('onama');
